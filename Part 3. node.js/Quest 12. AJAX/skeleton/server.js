a16997d5d4e8de502c98b73cd0e26cb009a03721
Lim Beom-Su <lbs1163@postech.ac.kr>
2017-05-01 01:19:30 +0900

    clear Quest 12

--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -1,15 +1,47 @@
 var express = require('express'),
 	path = require('path'),
-	app = express();
+	app = express(),
+	fs = require('fs'),
+	bodyParser = require('body-parser');
 
 app.use(express.static('client'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/note', function (req, res) {
+	var id = req.query.id;
+
+	fs.readFile('note' + id + '.txt', function(err, data) {
+		var d;
+
+		if (err)
+			d = "";
+		else
+			d = data;
+
+		res.send(d);
+	});
+});
+
+app.post('/note', function (req, res) {
+	var id = req.query.id;
+
+	fs.writeFile('note' + id + '.txt', req.body['data'], "utf8", function(err) {
+		var d;
+
+		if (err) d = err;
+		else d = 'note' + id + '.txt is saved';
+		console.log(d);
+
+		res.send(d);
+	});
+});
+
 /* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
 
-var server = app.listen(8080, function () {
+var server = app.listen(8000, function () {
 	console.log('Server started!');
 });
